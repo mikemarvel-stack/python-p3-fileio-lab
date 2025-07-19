07a6e07e3b68c1fe44224c9915474ad9ce279758
michael <michaelmuturi15@gmail.com>
2025-07-19 17:16:51 +0300

    file_io.py updated

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
+import os
 def write_file(file_name, file_content):
-    pass
+    path = f"{file_name}.txt"
+    directory = os.path.dirname(path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(path, 'w') as f:
+        f.write(file_content) 
+        pass
 
 def append_file(file_name, append_content):
+    path = f"{file_name}.txt"
+    directory = os.path.dirname(path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(path, 'a') as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    path = f"{file_name}.txt"
+    with open(path, 'r') as f:
+        return f.read()
     pass
